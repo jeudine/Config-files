649ef7cf6d4a4a86db1d886de0d99e51aab553c4
julien <julien@eudine.fr>
2025-03-01 16:31:01 +0100

    keyboard updated

--- keyboard/julien/keymap.c
+++ keyboard/julien/keymap.c
@@ -25,50 +25,38 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [LAYER_LETTERS] = LAYOUT_split_3x6_3(
   //,-----------------------------------------------------.                    ,-----------------------------------------------------.
-       KC_NO,    KC_Q,    KC_W,    KC_E,    KC_R,    LT(LAYER_NUMBERS, KC_T),    KC_Y,    KC_U,    KC_I,    KC_O,   KC_P,  KC_NO,
+     KC_NO,   KC_Q,    KC_W,    KC_E,    KC_R,    LT(LAYER_NUMBERS, KC_T),      KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_NO,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-      KC_NO,    KC_A,    KC_S,    KC_D,    KC_F,    LT(LAYER_SIGNS, KC_G),      KC_H,    KC_J,    KC_K,    KC_L,    KC_BSPC, KC_NO,
+     KC_NO,   KC_A,    KC_S,    KC_D,    KC_F,    LT(LAYER_SIGNS, KC_G),        KC_H,    KC_J,    KC_K,    KC_L,    KC_BSPC, KC_NO,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-      KC_NO,    KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,                         KC_N,    KC_M, KC_COMM,  KC_DOT,  KC_ENT,  KC_NO,
+     KC_NO,   KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,                         KC_N,    KC_M,    KC_COMM,   KC_ESC, KC_ENT,  KC_NO,
   //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
-                                        KC_TAB, KC_LEFT_SHIFT, KC_LCTL,  KC_RGUI, KC_LALT, KC_SPC
+                                         KC_LALT, KC_LEFT_SHIFT, KC_TAB,KC_SPC, KC_RGUI, KC_RCTL
                                       //`--------------------------'  `--------------------------'
 
   ),
 
     [LAYER_SIGNS] = LAYOUT_split_3x6_3(
   //,-----------------------------------------------------.                    ,-----------------------------------------------------.
-       KC_NO,    KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                         KC_0,    KC_1,    KC_2,    KC_3,    KC_4, KC_NO,
+     KC_NO,   KC_BSLS, KC_LBRC, KC_RBRC, KC_NO,   KC_NO,                        KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_TILD, KC_NO,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-      KC_NO, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                      KC_LEFT, KC_DOWN,   KC_UP,KC_RIGHT, XXXXXXX, KC_NO,
+     KC_NO,   KC_SCLN, KC_QUOT, KC_QUES, KC_NO,   KC_NO,                        KC_LEFT,  KC_DOWN, KC_UP, KC_RIGHT, KC_UNDS, KC_NO,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-      KC_NO, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                      KC_1, KC_2, XXXXXXX, XXXXXXX, XXXXXXX, KC_NO,
+     KC_NO,   KC_LABK, KC_RABK, KC_DLR,  KC_NO,   KC_NO,                        KC_EXLM, KC_AT,   KC_HASH, KC_DOT,  KC_CIRC, KC_NO,
   //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
-                                          KC_LGUI, _______,  KC_SPC,     KC_ENT,   MO(3), KC_RALT
+                                          KC_TRNS, KC_TRNS, KC_TRNS,   KC_TRNS, KC_TRNS, KC_TRNS
                                       //`--------------------------'  `--------------------------'
   ),
 
     [LAYER_NUMBERS] = LAYOUT_split_3x6_3(
   //,-----------------------------------------------------.                    ,-----------------------------------------------------.
-       KC_TAB, KC_EXLM,   KC_AT, KC_HASH,  KC_DLR, KC_PERC,                      KC_7, KC_8, KC_9, KC_LPRN, KC_RPRN, KC_BSPC,
+     KC_NO,   KC_PIPE, KC_LCBR, KC_RCBR, KC_NO,   KC_NO,                        KC_7,    KC_8,    KC_9,    KC_0,    KC_EQL,  KC_NO,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-      KC_LCTL, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                      KC_4,  KC_5, KC_6, KC_RBRC, KC_BSLS,  KC_GRV,
+     KC_NO,   KC_COLN, KC_DQUO, KC_NO,   KC_NO,   KC_NO,                        KC_4,    KC_5,    KC_6,    KC_MINS, KC_SLSH, KC_NO,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-      KC_LSFT, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                      KC_1, KC_2, KC_3, KC_RCBR, KC_PIPE, KC_TILD,
+     KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,                        KC_1,    KC_2,    KC_3,    KC_PLUS, KC_PERC, KC_NO,
   //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
-                                          KC_LGUI,   MO(3),  KC_SPC,     KC_ENT, _______, KC_RALT
-                                      //`--------------------------'  `--------------------------'
-  ),
-
-    [3] = LAYOUT_split_3x6_3(
-  //,-----------------------------------------------------.                    ,-----------------------------------------------------.
-      QK_BOOT, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                      XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
-  //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-      RM_TOGG, RM_HUEU, RM_SATU, RM_VALU, XXXXXXX, XXXXXXX,                      XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
-  //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-      RM_NEXT, RM_HUED, RM_SATD, RM_VALD, XXXXXXX, XXXXXXX,                      XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
-  //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
-                                          KC_LGUI, _______,  KC_SPC,     KC_ENT, _______, KC_RALT
+                                          KC_TRNS, KC_TRNS, KC_TRNS,   KC_TRNS, KC_TRNS, KC_TRNS
                                       //`--------------------------'  `--------------------------'
   )
 };
